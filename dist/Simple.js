2b615a04f1e89c54cbab548da28536cbd1e70c4c
Gheric Santiago <ghericsantiago@gmail.com>
2019-08-15 15:53:23 +0800

    Initial Commit

---
+++ dist/Simple.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Simple = props => {
+  return React.createElement("div", null, "Simple");
+};
+
+export default Simple;
---
+++ src/Simple.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Simple = props => {
+    return (
+        <div>Simple</div>
+    );
+}
+
+export default Simple;
